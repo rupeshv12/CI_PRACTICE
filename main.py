4704bb5d3c924e08c8c393425efb03a2e6d2f706
Pranjal Yadav <godricglow@gmail.com>
2023-12-01 16:11:58 +0530

    Moving to cml with plotting related updates

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-import numpy as np
+import matplotlib.pyplot as plt
 
-print('Hello World')
+fig, ax = plt.subplots()
 
-print(f'Some random fraction from numpy is {np.random.random()}')
+fruits = ['apple', 'blueberry', 'cherry', 'orange']
+counts = [40, 100, 30, 55]
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+plt.savefig('bars.png', bbox_inches='tight')
+
+cat = ["bored", "happy", "bored", "bored", "happy", "bored"]
+dog = ["happy", "happy", "happy", "happy", "bored", "bored"]
+activity = ["combing", "drinking", "feeding", "napping", "playing", "washing"]
+
+fig, ax = plt.subplots()
+ax.plot(activity, dog, label="dog")
+ax.plot(activity, cat, label="cat")
+ax.legend()
+
+plt.savefig('lines.png', bbox_inches='tight')
