6175023d0213601aaab5492a1dc9adf34fa9c375
Pranjal Yadav <godricglow@gmail.com>
2023-12-10 21:38:36 +0530

    changed fruit count and changed dog mood

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 fruits = ['apple', 'blueberry', 'cherry', 'orange']
-counts = [40, 100, 30, 55]
+counts = [140, 10, 30, 55]
 bar_labels = ['red', 'blue', '_red', 'orange']
 bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
 
@@ -16,7 +16,7 @@ ax.legend(title='Fruit color')
 plt.savefig('bars.png', bbox_inches='tight')
 
 cat = ["bored", "happy", "happy", "happy", "happy", "bored"]
-dog = ["happy", "happy", "happy", "happy", "bored", "bored"]
+dog = ["bored", "bored", "bored", "happy", "bored", "bored"]
 activity = ["combing", "drinking", "feeding", "napping", "playing", "washing"]
 
 fig, ax = plt.subplots()
